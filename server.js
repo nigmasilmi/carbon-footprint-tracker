e2ad64a4e80cd14305409c19529ef8d97a86101b
nigmasilmi <nigmasilmi.web@gmail.com>
2020-11-29 19:40:30 -0300

    prepare for deploy

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
+const path = require('path');
 const express = require('express');
 const connectDB = require('./config/db');
+
 
 const app = express();
 
@@ -10,7 +12,6 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.json({ msg: 'welcome to track your carbon footprint' }));
 
 // Define routes
 app.use('/api/users', require('./routes/users'));
@@ -20,6 +21,13 @@ app.use('/api/auth', require('./routes/auth'));
 // PORT
 const PORT = process.env.PORT || 5000;
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 app.listen(PORT, () => console.log(`conecting to the server on port ${PORT}`));
 
